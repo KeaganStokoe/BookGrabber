c22a3cf16d68b01fbcad65e8facf6dde6501b69a
Keagan-EXPLORE <keagan@explore-ai.net>
2022-01-26 08:45:49 +0200

    request data from GoogleBooks API

--- popup.js
+++ popup.js
@@ -10,10 +10,21 @@ changeColor.addEventListener("click", async () => {
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
+const setPageBackgroundColor = async () => {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
   var elements = document.getElementsByTagName("title");
-  console.log(elements[0].innerHTML);
-}
+  let title = elements[0].innerHTML;
+  console.log(title);
+
+  // make request to API
+  let bookData = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${title}`
+  );
+  //use string literals
+  let bookDataJson = await bookData.json();
+  console.log(bookDataJson["items"][0]["volumeInfo"]);
+
+  // The log above has the info you need so you can just make the Notion API post request now
+};
